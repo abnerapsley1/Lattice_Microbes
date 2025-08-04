cc7ade3d62ada02697b9aef49ee599e3bc7f7f5c
abnerapsley1 <57500819+abnerapsley1@users.noreply.github.com>
2025-08-04 11:56:54 -0500

    Update LMLogger.py

--- src/jlm/jLM/LMLogger.py
+++ src/jlm/jLM/LMLogger.py
@@ -38,12 +38,22 @@
 # 
 #
 
+
+##############################################
+### Importing Additional Necessary Modules ###
+##############################################
 import logging
 
-# Create the default LM Logger
+
+### Create the Default LM Logger ###
+# Create object LMLogger with module getLogger() method and name it 'LMLogger' #
 LMLogger = logging.getLogger('LMLogger')
 
-# Gracefully catch logging.NullHandler() not existing in python 2.6
+# Add a null handler object to LMLogger to ensure no "No handlers could be found... " warnings #
+# The handlers are simply where the logging messages will be printed or saved #
+# Adding a null handler means they will not be saved anywhere, but there will be no errors #
+# due to the logger not being able to find a handler #
+# Gracefully catch logging.NullHandler() not existing in python 2.6 and adding a new class if not available #
 try:
 	nullHandlerLM = logging.NullHandler()
 except AttributeError:
@@ -51,39 +61,62 @@ except AttributeError:
 		def emit(self, record):
 			pass
 	nullHandlerLM = NullHandler()
+# Add the null handler to the LMLogger object #
+LMLogger.addHandler(nullHandlerLM)
 
-LMLogger.addHandler(nullHandlerLM) # Set the library to initially print to nothing
-
-# Set up the formatter
+# Define the logging formatter (LMformatter) using logging.Formatter() method #
+# Define the format to print the time, level, and message contents #
 LMformatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s')
+# Set the null handler format as LMformatter #
 nullHandlerLM.setFormatter(LMformatter)
 
 
 
 
-
+### Define setLMLoggerLevel Function ###
 def setLMLoggerLevel(level):
-	"""Set the level of the logger for the application
+        """
+        Set the level of the logger for the application
 
-    Args:
-        level:
-            The level the logger should report (e.g. logger.WARNING, logger.INFO, etc.)
-    """
-	LMLogger.setLevel(level)
+        This function enables the user to set the logger level (i.e., the category
+        the user wants the message to be assigned). This is useful if you have 
+	a lot of logging messages to print of the same level.
+
+        Args:
+            level (string): REQUIRED, the level the logger should report (e.g. logger.WARNING, 
+	        logger.INFO, etc.)
+
+        Returns:
+            none
+
+        """
+	
+        LMLogger.setLevel(level)
 
 
 
 
 
 def setLMLogFile(filename, level=logging.DEBUG):
-	"""Set up file handler to print log to file
+        """
+        Set the file name for logging the information
 
-    Args:
-        filename:
-            The name of the file to log information
-        level:
-            The level of information to log
-    """
+        This function enables the user to set the logger file which will
+	specify the file name where all logging information is printed/saved.
+
+        Args:
+            filename (string): REQUIRED, the name of the file for the logging information
+	        to be stored.
+	    level (string): The level for the logger to set the messages at. This needs
+                to be in the following format "logging.LEVEL", where LEVEL can be DEBUG,
+		INFO, WARNING, ERROR, CRITICAL, etc.
+
+        Returns:
+            none
+
+        """
+
+	
 	fileH = logging.FileHandler(filename, mode='w')
 	fileH.setLevel(level)
 	fileH.setFormatter(LMformatter)
